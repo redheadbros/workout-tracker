faa9441da6f7f44ec48fc32e3fd16faa6e589836
Luke Rostad <rostad@ualberta.ca>
2020-01-10 11:08:16 -0700

    added edit workout button; changed location and look of circle buttons

--- app/src/main/java/com/example/workouttracker/WorkoutDescription.java
+++ app/src/main/java/com/example/workouttracker/WorkoutDescription.java
@@ -27,6 +27,15 @@ public class WorkoutDescription extends AppCompatActivity {
             .setAction("Action", null).show();
       }
     });
+
+    FloatingActionButton fab2 = findViewById(R.id.fab2);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
   }
 
 }
