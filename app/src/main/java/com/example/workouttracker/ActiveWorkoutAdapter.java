58e1c99ded4b878f2a0d37c65c35f9ec4f272819
Luke Rostad <rostad@ualberta.ca>
2020-01-18 16:22:00 -0700

    just finished ActiveWorkoutAdapter, think it works. RecyclerView within RecyclerView is new to me.

--- app/src/main/java/com/example/workouttracker/ActiveWorkoutAdapter.java
+++ app/src/main/java/com/example/workouttracker/ActiveWorkoutAdapter.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.workouttracker.datastructure.Cycle;
@@ -16,21 +19,23 @@ import java.util.ArrayList;
 public class ActiveWorkoutAdapter extends RecyclerView.Adapter<ActiveWorkoutAdapter.ActiveWorkoutViewHolder> {
 
   public static class ActiveWorkoutViewHolder extends RecyclerView.ViewHolder {
-    public LinearLayout view;
-    public ActiveWorkoutViewHolder(LinearLayout v) {
-      super(v);
-      view = v;
+    public LinearLayout linearLayout;
+    public ActiveWorkoutViewHolder(LinearLayout l) {
+      super(l);
+      linearLayout = l;
     }
   }
 
   private Context context;
   private Workout workout;
   private ArrayList<Cycle> cycles;
+  private WorkoutProgress progress;
 
   public ActiveWorkoutAdapter(Context appContext, Workout workoutToDo) {
     context = appContext;
     workout = workoutToDo;
     cycles = workoutToDo.getCycles();
+    progress = new WorkoutProgress(workoutToDo);
   }
 
   @Override
@@ -45,7 +50,59 @@ public class ActiveWorkoutAdapter extends RecyclerView.Adapter<ActiveWorkoutAdap
 
   @Override
   public void onBindViewHolder(ActiveWorkoutViewHolder holder, final int position) {
+    //set title of textview
+    LinearLayout cycleTitleLayout = (LinearLayout) holder.linearLayout.getChildAt(0);
+    TextView cycleTitleTextView = (TextView) cycleTitleLayout.getChildAt(0);
 
+    //add on the number of repetitions, i.e. Cycle Name (x3)
+    Cycle currentCycle = cycles.get(position);
+    String cycleTitleText = currentCycle.getName() + " (x";
+    cycleTitleText += currentCycle.getCycleRepetitions() + ")";
+
+    cycleTitleTextView.setText(cycleTitleText);
+
+    //set counter correctly
+    LinearLayout cycleTitleCounterLayout = (LinearLayout) cycleTitleLayout.getChildAt(1);
+    TextView cycleTitleCounterTextView = (TextView) cycleTitleCounterLayout.getChildAt(1);
+    cycleTitleCounterTextView.setText(progress.getCyclesCompleted(position));
+
+    //setup counter buttons
+    Button minus = (Button) cycleTitleCounterLayout.getChildAt(0);
+    Button plus = (Button) cycleTitleCounterLayout.getChildAt(2);
+
+    minus.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        //change progress
+        progress.setCyclesCompleted(position, progress.getCyclesCompleted(position) - 1);
+
+        //update text view
+        LinearLayout cycleCounterLayout = (LinearLayout) v.getParent();
+        TextView cycleCounterTextView = (TextView) cycleCounterLayout.getChildAt(1);
+        cycleCounterTextView.setText(progress.getCyclesCompleted(position));
+      }
+    });
+
+    plus.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        //change progress
+        progress.setCyclesCompleted(position, progress.getCyclesCompleted(position) + 1);
+
+        //update text view
+        LinearLayout cycleCounterLayout = (LinearLayout) v.getParent();
+        TextView cycleCounterTextView = (TextView) cycleCounterLayout.getChildAt(1);
+        cycleCounterTextView.setText(progress.getCyclesCompleted(position));
+      }
+    });
+
+    //setup recyclerView
+    RecyclerView exerciseView = (RecyclerView) holder.linearLayout.getChildAt(1);
+    LinearLayoutManager layoutManager = new LinearLayoutManager(context);
+    exerciseView.setLayoutManager(layoutManager);
+
+    ActiveCycleAdapter adapter = new ActiveCycleAdapter(context, cycles.get(position));
+    exerciseView.setAdapter(adapter);
   }
 
   public int getItemCount() {
