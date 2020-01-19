387b022f94ff7eb3908a0a96d46daeec85348722
Kariba-2001 <nwaezeap@ualberta.ca>
2020-01-18 18:26:42 -0700

    chnaged splash from 5 seconds to 2 seconds

--- app/src/main/java/com/example/workouttracker/History.java
+++ app/src/main/java/com/example/workouttracker/History.java
@@ -42,7 +42,7 @@ public class History extends AppCompatActivity {
 
         dateList = new ArrayList<String>();
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dateList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dateList);
 
 
         imageView = (ImageView)findViewById(R.id.imageView);
@@ -58,7 +58,19 @@ public class History extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                openWorkoutDescription();
+
+                Bundle bundle = new Bundle();
+
+                if(position == 0){
+                    Intent intent = new Intent(History.this, ActiveWorkout.class);
+                    startActivity(intent);
+                }
+
+                else if(position == 1){
+                   Intent intent = new Intent(History.this, ActiveWorkout.class);
+                   startActivity(intent);
+
+                }
 
             }
         });
@@ -75,6 +87,7 @@ public class History extends AppCompatActivity {
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
+                                clearHistory();
 
                             }
                         })
@@ -105,7 +118,7 @@ public class History extends AppCompatActivity {
     }
 
     public void generateAllDate(){
-        HistoryData historyData = Json.loadFromJson(getApplicationContext(), HistoryData.class,"HISTORY,json");
+        HistoryData historyData = Json.loadFromJson(getApplicationContext(), HistoryData.class,"HISTORY.json");
         if(historyData == null){
             return;
         }
@@ -115,6 +128,7 @@ public class History extends AppCompatActivity {
             DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
             String strDate = dateFormat.format(date);
             dateList.add(strDate);
+            index++;
         }
     }
 
@@ -127,12 +141,13 @@ public class History extends AppCompatActivity {
         while(index < historyData.getHistoryList().size()){
             Workout workout = historyData.getWorkout(index);
             workoutList.add(workout);
+            index++;
         }
 
     }
 
     public void openWorkoutDescription(){
-        generateAllWorkout();
+
     }
 
     public void clearHistory(){
--- app/src/main/java/com/example/workouttracker/splash_screen.java
+++ app/src/main/java/com/example/workouttracker/splash_screen.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class splash_screen extends AppCompatActivity {
-private static int SPLASH_TIME_OUT = 5000;
+private static int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
