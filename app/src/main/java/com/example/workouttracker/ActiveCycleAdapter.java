67eff58531c7c3713e683a546e5d507e3be0eb04
Luke Rostad <rostad@ualberta.ca>
2020-01-20 10:16:33 -0700

    added documentation for ActiveCycleAdapter

--- app/src/main/java/com/example/workouttracker/ActiveCycleAdapter.java
+++ app/src/main/java/com/example/workouttracker/ActiveCycleAdapter.java
@@ -12,6 +12,24 @@ import com.example.workouttracker.datastructure.Workout;
 
 import java.util.ArrayList;
 
+/**
+ * ActiveCycleAdapter: lays out the elements of one cycle of "Active Workout"
+ *
+ * ActiveExerciseViewHolder (class): holds the layout of one exercise, and provides
+ *   references to different parts of said layout.
+ *
+ * onCreateViewHolder(ViewGroup, int) -> ActiveExerciseViewHolder:
+ *   creates a new custom view holder from its layout file.
+ *
+ * onBindViewHolder(ActiveExerciseViewHolder, int):
+ *   applies the correct details to the layout, and sets up the set counter.
+ *
+ * onViewRecycled(ActiveExerciseViewHolder):
+ *   reads the current counter value and stores it until this view returns.
+ *
+ * getItemCount() -> int:
+ *   returns the number of exercises in this cycle.
+ */
 public class ActiveCycleAdapter extends RecyclerView.Adapter<ActiveCycleAdapter.ActiveExerciseViewHolder> {
 
   private static final int EXERCISE_TITLE_LAYOUT_INDEX = 0;
@@ -19,6 +37,10 @@ public class ActiveCycleAdapter extends RecyclerView.Adapter<ActiveCycleAdapter.
   private static final int EXERCISE_TITLE_VIEW_INDEX = 0;
   private static final int EXERCISE_COUNTER_LAYOUT = 1;
 
+  /**
+   * ActiveExerciseViewHolder (class): holds the layout of one exercise, and provides
+   *   references to different parts of said layout.
+   */
   public static class ActiveExerciseViewHolder extends RecyclerView.ViewHolder {
     public LinearLayout exerciseLayout;
     public LinearLayout titleLayout;
@@ -51,6 +73,13 @@ public class ActiveCycleAdapter extends RecyclerView.Adapter<ActiveCycleAdapter.
     progress = workoutProgress;
   }
 
+  /**
+   * onCreateViewHolder(ViewGroup, int) -> ActiveExerciseViewHolder:
+   *   creates a new custom view holder from its layout file.
+   * @param parent: the view group this will be a part of.
+   * @param viewType: the view type (unused)
+   * @return: a new view holder.
+   */
   @Override
   public ActiveExerciseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
@@ -61,6 +90,12 @@ public class ActiveCycleAdapter extends RecyclerView.Adapter<ActiveCycleAdapter.
     return holder;
   }
 
+  /**
+   * onBindViewHolder(ActiveExerciseViewHolder, int):
+   *   applies the correct details to the layout, and sets up the set counter.
+   * @param holder: the view holder to be set up.
+   * @param position: index of the exercise to be displayed.
+   */
   @Override
   public void onBindViewHolder(ActiveExerciseViewHolder holder, final int position) {
     //store position data
@@ -77,6 +112,11 @@ public class ActiveCycleAdapter extends RecyclerView.Adapter<ActiveCycleAdapter.
     CustomCounterHelper.setupSetCounter(holder.counterLayout, workout, progress, cycleIndex, position);
   }
 
+  /**
+   * onViewRecycled(ActiveExerciseViewHolder):
+   *   reads the current counter value and stores it until this view returns.
+   * @param holder: the holder for the to-be-recycled view.
+   */
   @Override
   public void onViewRecycled(ActiveExerciseViewHolder holder) {
     TextView counterTextView = (TextView) holder.counterLayout.getChildAt(CustomCounterHelper.COUNTER_TEXT_INDEX);
@@ -84,6 +124,11 @@ public class ActiveCycleAdapter extends RecyclerView.Adapter<ActiveCycleAdapter.
     progress.setSetsCompleted(cycleIndex, holder.exerciseIndex, setsCompleted);
   }
 
+  /**
+   * getItemCount() -> int:
+   *   returns the number of exercises in this cycle.
+   * @return: the number of exercises.
+   */
   @Override
   public int getItemCount() {
     return exercises.size();
